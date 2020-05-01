cc9b6bb449c43868984f3012c18bc262bdefc78f
LeodeWill <leode.g.will@gmail.com>
2020-05-01 00:17:25 -0300

    home page layout basics

---
+++ petshop/src/components/Toolbar.vue
@@ -0,0 +1,51 @@
+<template>
+    <nav>
+        <v-toolbar flat app class="blue lighten-4">
+            <div>
+                <v-toolbar-title class="text-uppercase blue--text">
+                    <span>Petshop</span>
+                </v-toolbar-title>
+            </div>
+
+            <v-spacer></v-spacer>
+
+            <div v-show="logged">
+                <v-btn text color="blue">
+                    <span>Profile</span>
+                </v-btn>
+
+                <v-btn text color="blue">
+                    <span>Services</span>
+                </v-btn>
+
+                <v-btn text color="blue" @click="logged = false">
+                    <span>Sign Out</span>
+                </v-btn>
+
+                <v-btn text color="blue">
+                    <span>Cart</span>
+                </v-btn>
+            </div>
+            <div v-show="!logged">
+                <v-btn text color="blue" @click="logged = true">
+                    <span>Sign In</span>
+                </v-btn>
+            </div>
+
+            
+
+        </v-toolbar>
+        
+    </nav>
+    
+</template>
+
+<script>
+export default{
+    data() {
+        return{
+            logged: true
+        }
+    }
+}
+</script>
--- petshop/src/views/Home.vue
+++ petshop/src/views/Home.vue
@@ -1,13 +1,78 @@
 <template>
   <div class="home">
-    <h1>Homepage</h1>
+    <h1>Products</h1>
+    <v-container>
+
+      <v-layout row wrap>
+        <v-flex md2 v-for="product in storage" :key="product.name">
+          <v-card flat class="text-center ma-3" color="blue lighten-5">
+            <v-responsive class="pt-3">
+              Imagem
+            </v-responsive>
+
+            <v-card-text>
+              <div class="subheading"> {{product.name}} </div>
+              <div right class="blue--text"> R$ {{product.price}} </div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn text small left color="blue">
+                <span>Buy</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+
+    </v-container>
+
+
+
+    <h1>Services</h1>
+    <v-container>
+
+      <v-layout row wrap>
+        <v-flex md2 v-for="service in services" :key="service.name">
+          <v-card flat class="text-center ma-3" color="blue lighten-5">
+            <v-responsive class="pt-3">
+              Imagem
+            </v-responsive>
+
+            <v-card-text>
+              <div class="subheading"> {{service.name}} </div>
+              <div right class="blue--text"> R$ {{service.price}} </div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn text small left color="blue">
+                <span>Schedule</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+      
+    </v-container>
+
   </div>
 </template>
 
 <script>
-
-
 export default {
-  
+  data() {
+        return{
+            storage: [
+              { name: 'A', price: 1.0},
+              { name: 'B', price: 1.0},
+              { name: 'C', price: 1.0}
+            ],
+
+            services: [
+              { name: 'X', price: 10.0},
+              { name: 'Y', price: 10.0},
+              { name: 'X', price: 10.0}
+            ]
+        }
+    }
 }
 </script>
