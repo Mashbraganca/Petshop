b4855b029171bdfb8b1f52d556ea3ce18a44adf0
Mashbraganca <mashbraganca@gmail.com>
2020-05-02 00:24:40 -0300

    Almooost oveeeerr

--- petshop/src/components/NavbarAdm.vue
+++ petshop/src/components/NavbarAdm.vue
@@ -10,7 +10,7 @@
 
             <v-toolbar-title>
                 <span class="font-weight-black">Pet Shop:</span>
-                <span class="font-weight-light">  Conta de admnistrador</span> 
+                <span class="font-weight-light">  Conta de administrador</span> 
             </v-toolbar-title>
 
             <v-spacer></v-spacer>
@@ -38,17 +38,182 @@
             </v-list-item>
             <v-divider></v-divider>
 
-            <v-list-group
-            prepend-icon="mdi-account-circle"
-            value="true"
-            >
-           <template v-slot:activator>
-                <v-list-item-title>
-                Usuários 
-                </v-list-item-title>
-            </template>
-            </v-list-group>
-           
+            <v-list rounded>
+                <v-list-group
+                prepend-icon="mdi-account-circle"
+                value="true"
+                color="deep-purple darken-4"
+                >
+                    <template v-slot:activator>
+                        <v-list-item-title>
+                        Usuários 
+                        </v-list-item-title>
+                    </template>
+
+                    <v-list-group
+                    no-action
+                    sub-group
+                    value="true"
+                    color="deep-purple darken-4"
+                    >
+                    
+                        <template v-slot:activator>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                    Administradores  
+                                </v-list-item-title>
+                            </v-list-item-content>
+                        </template>
+
+                        <v-list-item link>
+                            <v-list-item-tittle>Registrar</v-list-item-tittle>
+                            <v-list-item-icon>
+                                <v-icon>mdi-clipboard-account-outline</v-icon>
+                            </v-list-item-icon>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-tittle>Gerenciar</v-list-item-tittle>
+                            <v-list-item-icon>
+                                <v-icon>mdi-account-box-multiple</v-icon>
+                            </v-list-item-icon>
+                        </v-list-item>
+
+                    </v-list-group>
+
+                    <v-list-group
+                    no-action
+                    sub-group
+                    value="true"
+                    color="deep-purple darken-4"
+                    >
+                    
+                        <template v-slot:activator>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                    Clientes  
+                                </v-list-item-title>
+                            </v-list-item-content>
+                        </template>
+
+                        <v-list-item link>
+                            <v-list-item-tittle>Registrar</v-list-item-tittle>
+                            <v-list-item-icon>
+                                <v-icon>mdi-clipboard-account-outline</v-icon>
+                            </v-list-item-icon>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-tittle>Gerenciar</v-list-item-tittle>
+                            <v-list-item-icon>
+                                <v-icon>mdi-account-box-multiple</v-icon>
+                            </v-list-item-icon>
+                        </v-list-item>
+
+                    </v-list-group>
+
+
+                </v-list-group>
+
+                    <v-list-group
+                    prepend-icon="mdi-package"
+                    value="true"
+                    color="deep-purple darken-4"
+                    >
+
+                    <template v-slot:activator>
+                        <v-list-item-tittle>
+                            Produtos
+                        </v-list-item-tittle>
+                    </template>
+                    <v-list-group
+                    no-action
+                    sub-group
+                    value="true"
+                    color="deep-purple darken-4"
+                    >
+                        <template v-slot:activator>
+                            <v-list-item-content>
+                                <v-list-item-tittle>Categorias</v-list-item-tittle>
+                            </v-list-item-content>
+                        </template>
+
+                        <v-list-item link>
+                            <v-list-item-tittle>Criar</v-list-item-tittle>
+                            <v-list-item-icon>
+                                <v-icon>mdi-clipboard-account-outline</v-icon>
+                            </v-list-item-icon>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-tittle>Gerenciar</v-list-item-tittle>
+                            <v-list-item-icon>
+                                <v-icon>mdi-account-box-multiple</v-icon>
+                            </v-list-item-icon>
+                        </v-list-item>   
+                    </v-list-group> 
+
+                    <v-list-group
+                    no-action
+                    sub-group
+                    value="true"
+                    color="deep-purple darken-4"
+                    >
+                        <template v-slot:activator>
+                            <v-list-item-content>
+                                <v-list-item-tittle>Item</v-list-item-tittle>
+                            </v-list-item-content>
+                        </template>
+                        <v-list-item-group>
+                            <v-list-item link>
+                                <v-list-item-tittle>Criar</v-list-item-tittle>
+                                <v-list-item-icon>
+                                    <v-icon>mdi-clipboard-account-outline</v-icon>
+                                </v-list-item-icon>
+                            </v-list-item>
+                        </v-list-item-group>
+                        <v-list-item-group>
+                            <v-list-item link>
+                                <v-list-item-tittle>Gerenciar</v-list-item-tittle>
+                                <v-list-item-icon>
+                                    <v-icon>mdi-account-box-multiple</v-icon>
+                                </v-list-item-icon>
+                            </v-list-item>
+                        </v-list-item-group>   
+                    </v-list-group>
+
+
+                </v-list-group>
+
+                <v-list-group
+                prepend-icon="mdi-dog-service"
+                value="true"
+                color="deep-purple darken-4"
+                >
+                    <template v-slot:activator>
+                        <v-list-item-tittle>
+                            Serviços
+                        </v-list-item-tittle>
+                    </template>
+
+                </v-list-group>
+            
+                
+                <v-list-item-group
+                color="deep-purple darken-4"
+                >
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-cash</v-icon>
+                        </v-list-item-icon>
+
+                        <v-list-item-content>
+                            <v-list-item-title>Faturamentos</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+                
+            </v-list>
         </v-navigation-drawer>
         
     </nav>
