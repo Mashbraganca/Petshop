de96dea412182b2faa35ff64e8042e3e31fbc209
Mashbraganca <mashbraganca@gmail.com>
2020-05-05 19:30:50 -0300

    Admin register

--- petshop/src/views/Admin.vue
+++ petshop/src/views/Admin.vue
@@ -1,16 +1,206 @@
 <template>
-  <v-app>
+  <v-app style=" background: #B388FF background: -webkit-linear-gradient(to bottom, #EA80FC, #82B1FF, #B388FF); 
+background: linear-gradient(to bottom, #EA80FC, #82B1FF, #B388FF);">
     <NavbarAdm/>
-    <div class="admin" background-color="purple accent-4">
-      <div color="purple accent-4">
+    <div class="admin">
+      <div><v-row>a</v-row></div>
+      <div>
         <v-card 
-        class="mx-auto"
-        elevation-12
+        class="ma-auto"
+        elevation-15
         max-width="900"
         >
-          <v-list-item >
-            
-          </v-list-item>
+          <v-card-title primary-title>
+            Registrar novo administrador:
+          </v-card-title>
+          <v-form v-model="valid">
+            <v-container grid-list-xs>
+              <v-row>
+                <v-col
+                  cols="12"
+                  md="6"
+                >
+                  <v-text-field 
+                  v-model="name"
+                  :rules="nameRules"
+                  :counter="15"
+                  label="Nome"
+                  required
+                  ></v-text-field>
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  md="6"
+                >
+                  <v-text-field 
+                  v-model="lastname"
+                  :rules="lastnameRules"
+                  :counter="30"
+                  label="Sobrenome"
+                  required
+                  ></v-text-field>
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  md="6"
+                >
+                  <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                    label="E-mail"
+                    required
+                  ></v-text-field>
+
+                </v-col>
+                
+                <v-col
+                  cols="12"
+                  md="6"
+                >
+                  <v-text-field
+                    v-model="phone"
+                    :rules="phoneRules"
+                    label="Telefone"
+                    required
+                  ></v-text-field>
+
+                </v-col>
+                
+                <v-col
+                  cols="12"
+                  md="3"
+                >
+                  <v-avatar size=160px>
+                    <img :src=photo alt="Profile Picture">
+                  </v-avatar>
+
+                </v-col>
+                <v-col
+                  cols="12"
+                  md="8"
+                >
+                  <v-file-input
+                    :rules="rules"
+                    accept="image/png, image/jpeg, image/bmp"
+                    placeholder="Escolha uma foto"
+                    prepend-icon="mdi-camera"
+                    label="Foto de Profile"
+                  ></v-file-input>
+                </v-col>
+
+                <v-col
+                cols="12"
+                md="8"
+                ></v-col>
+
+                <v-col
+                cols="12"
+                md="4"
+                >
+                  <v-btn large color="primary">Submeter</v-btn>
+                </v-col>
+
+
+              </v-row>
+            </v-container>
+          </v-form>
+
+        </v-card>
+      </div>
+      <div><v-row>a</v-row></div>
+      <div>
+        <v-card 
+        class="ma-auto"
+        elevation-15
+        max-width="900"
+        >
+          <v-card-title primary-title>
+            Registrar novo administrador:
+          </v-card-title>
+          <v-form v-model="valid">
+            <v-container grid-list-xs>
+              <v-row>
+                <v-col
+                  cols="12"
+                  md="6"
+                >
+                  <v-text-field 
+                  v-model="name"
+                  :rules="nameRules"
+                  :counter="15"
+                  label="Nome"
+                  required
+                  ></v-text-field>
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  md="6"
+                >
+                  <v-text-field 
+                  v-model="lastname"
+                  :rules="lastnameRules"
+                  :counter="30"
+                  label="Sobrenome"
+                  required
+                  ></v-text-field>
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  md="6"
+                >
+                  <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                    label="E-mail"
+                    required
+                  ></v-text-field>
+
+                </v-col>
+                
+                <v-col
+                  cols="12"
+                  md="6"
+                >
+                  <v-text-field
+                    v-model="phone"
+                    :rules="phoneRules"
+                    label="Telefone"
+                    required
+                  ></v-text-field>
+
+                </v-col>
+                
+                <v-col
+                  cols="12"
+                  md="3"
+                >
+                  <v-avatar size=160px>
+                    <img :src=photo alt="Profile Picture">
+                  </v-avatar>
+
+                </v-col>
+                <v-col
+                  cols="12"
+                  md="8"
+                >
+                  <v-file-input
+                    :rules="rules"
+                    accept="image/png, image/jpeg, image/bmp"
+                    placeholder="Escolha uma foto"
+                    prepend-icon="mdi-camera"
+                    label="Foto de Profile"
+                  ></v-file-input>
+                </v-col>
+
+
+              </v-row>
+            </v-container>
+          </v-form>
+
         </v-card>
       </div>
     </div>
@@ -27,7 +217,31 @@ export default {
   
 
   data: () => ({
-    //
+    valid: false,
+      firstname: '',
+      lastname: '',
+      nameRules: [
+        v => !!v || 'Requer nome',
+        v => v.length <= 15 || 'Nome tem que ter menos que 15 caracteres',
+      ],
+      lastnameRules: [
+        v => !!v || 'Requer sobrenome',
+        v => v.length <= 30 || 'Sobrenome tem que ter menos que 30 caracteres',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+      phone: '',
+      phoneRules: [
+        v => !!v || 'Requer número de telefone',
+        v => /^[0-9]*$/.test(v)   || 'Número tem que ser válido'
+      ],
+      photo: './profile-placeholder.png',
+      photoRule: [
+        value => !value || value.size < 2000000 || 'A foto de profile tem que ser menos de 2MB!',
+      ],
   }),
 };
 </script>
